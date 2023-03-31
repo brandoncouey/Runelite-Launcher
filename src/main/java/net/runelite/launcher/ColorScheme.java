47aec840466a6f243c4900b44a2e5fc0841ae932
Brandon Couey <brradc@gmail.com>
2023-03-30 22:04:20 -0500

    app icons

--- src/main/java/net/runelite/launcher/ColorScheme.java
+++ src/main/java/net/runelite/launcher/ColorScheme.java
@@ -4,11 +4,11 @@ import java.awt.Color;
 
 public class ColorScheme
 {
-	/* The blue color used for the branding's accents */
-	public static final Color BRAND = new Color(220, 138, 0);
+	/* The red color used for the branding's accents */
+	public static final Color BRAND = new Color(216, 56, 2);
 
-	/* The blue color used for the branding's accents, with lowered opacity */
-	public static final Color BRAND_TRANSPARENT = new Color(220, 138, 0, 120);
+	/* The red color used for the branding's accents, with lowered opacity */
+	public static final Color BRAND_TRANSPARENT = new Color(216, 56, 2, 120);
 
 
 	public static final Color DARK_GRAY_COLOR = new Color(40, 40, 40);
